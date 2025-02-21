902b04eb4805144e8428a4428fbbeec58c436c44
EzraPatrikha123 <ezrapatrikha123@gmail.com>
2025-02-21 12:56:36 +0700

    first

---
+++ database/migrations/2025_02_20_032156_create_transactions.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->integer('id')->autoIncrement();
+            $table->integer('users_id');
+            $table->date('transaction_date');
+            $table->timestamps();
+
+            $table->foreign('users_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
